5a7228aadd5926d42b57e4e0bcf9a2012caaa777
Angel Larios <lariosangel032@gmail.com>
2025-03-02 20:08:20 -0500

    feat(Modal): Se implementa loader en modal de crear tarea.

--- frontend/TaskManager.WEB/src/app/features/tasks/components/create-task-modal/create-task-modal.component.ts
+++ frontend/TaskManager.WEB/src/app/features/tasks/components/create-task-modal/create-task-modal.component.ts
@@ -3,16 +3,19 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { TasksService } from '../../../../core/services/tasks/tasks.service';
 import { AlertModalComponent } from '../../../../shared/components/alert-modal/alert-modal.component';
+import { LoaderComponent } from '../../../../shared/components/loader/loader.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-task-modal',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, LoaderComponent, CommonModule],
   templateUrl: './create-task-modal.component.html',
   styleUrl: './create-task-modal.component.scss'
 })
 export class CreateTaskModalComponent {
   taskForm: FormGroup;
+  loading = false;
 
   constructor(
     private fb: FormBuilder,
@@ -27,10 +30,12 @@ export class CreateTaskModalComponent {
   createTask(title: any): void {
     this.taskService.createTask(title).subscribe({
       next: () => {
+        this.loading = false;
         this.openAlertModal('Tarea creada exitosamente', 'success');
         this.dialogRef.close(true); 
       },
       error: () => {
+        this.loading = false;
         this.openAlertModal('Error al crear la tarea', 'error');
       }
     });
@@ -43,6 +48,7 @@ export class CreateTaskModalComponent {
     });
   }
   save(): void {
+    this.loading = true;
     var data = {
       title: this.taskForm.get('title')?.value
     }
